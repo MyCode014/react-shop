d85854025b4205e777e5841a965dd696a9d4981d
Maja Markovic <122278978+MyCode014@users.noreply.github.com>
2023-12-07 00:38:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-shop/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Postavite izlazni direktorij na 'dist'
+  },
 })
